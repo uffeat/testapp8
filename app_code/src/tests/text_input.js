f400ec43c0139bf91ea150a38f1b35e93220f52e
Uffe Arlo <uat@aperitas.com>
2024-11-24 15:13:18 +0100

    k

--- app_code/src/tests/text_input.js
+++ app_code/src/tests/text_input.js
@@ -2,10 +2,10 @@
 
 await (async () => {
   const { create } = await import("rollo/component");
-  const { ErrorFeedback } = await import("rolloui/form/ErrorFeedback");
+  const { Floating } = await import("rolloui/form/Floating");
+  const { InvalidFeedback } = await import("rolloui/form/InvalidFeedback");
   const { TextInput } = await import("rolloui/form/input/TextInput");
   const { Label } = await import("rolloui/form/Label");
-  const { Floating } = await import("rolloui/form/Floating");
 
   const form = create(
     "form.d-flex.flex-column.row-gap-3.p-3",
@@ -26,12 +26,17 @@ await (async () => {
           },
         ],
       }),
-      ErrorFeedback({ for_name: "my_name" })
+      InvalidFeedback({for_name: 'my_name'}),
     ),
     Floating(
       { label: "Foo" },
       TextInput({ name: "foo", required: true }),
-      ErrorFeedback({ for_name: "foo" })
+      InvalidFeedback(),
+      /* Connect invalid feedback to input; alternative to using for_name */
+      function () {
+        this.querySelector(".invalid-feedback").form_control =
+          this.querySelector("input");
+      }
     ),
     create(
       "DIV.input-group",
@@ -41,12 +46,8 @@ await (async () => {
         { label: "Bar" },
         TextInput({ name: "bar", required: true, ["css_rounded-end"]: true })
       ),
-      ErrorFeedback({ for_name: "bar" })
+      InvalidFeedback({for_name: 'bar'}),
+      
     ),
-    Floating(
-      { label: "Stuff" },
-      TextInput({ name: "stuff", required: true, type: "textarea" }),
-      ErrorFeedback({ for_name: "stuff" })
-    )
   );
 })();
