ebcf7c6e78b2b42f384300601e6515ede83c3b1e
Uffe Arlo <uat@aperitas.com>
2024-12-16 12:40:37 +0100

    k

--- app_code/src/main.js
+++ app_code/src/main.js
@@ -66,24 +66,24 @@ await (async () => {
   const my_sheet = document.querySelector(`css-sheet[name="my_sheet"]`);
   const my_rule = document.querySelector(`css-rule[name="my_rule"]`);
   const my_rule_clone = my_rule.clone();
-  const DELAY = 1000;
+  const DELAY = 3000;
+  const action_display = create("p", { parent: root, padding: "8px", text: 'Original CSS.' });
   const css_display = create("code", { parent: root, padding: "8px" });
 
-  const css_effect = () => {
-    console.log("css:", my_sheet.text);
-    css_display.text = my_sheet.text;
-  };
-
-  const target_effect = (current, previous, owner) => {
-    if (current.target) {
-      owner.effects.add(css_effect);
-    } else {
-      owner.effects.remove(css_effect);
+  new (class {
+    constructor(...owners) {
+      owners.forEach((owner) =>
+        owner.effects.add((current, previous, component) => {
+          if (current.target) {
+            component.effects.add(this.effect);
+          } else {
+            component.effects.remove(this.effect);
+          }
+        }, "target")
+      );
     }
-  }
-
-  my_rule.effects.add(target_effect, "target");
-  my_rule_clone.effects.add(target_effect, "target");
+    effect = () => (css_display.text = my_sheet.text);
+  })(my_rule, my_rule_clone);
 
   pipe(
     null,
@@ -91,41 +91,49 @@ await (async () => {
     async () => {
       await delay(DELAY);
       my_rule.update({ color: "green", selector: "h2" });
+      action_display.text = 'Updated color and selector; backgroundColor and padding remain the same.'
     },
     /* Update color; selector and all other items remain the same */
     async () => {
       await delay(DELAY);
       my_rule.$.color = "blue";
+      action_display.text = 'Updated color; selector and all other items remain the same.'
     },
     /* Reset rule completely */
     async () => {
       await delay(DELAY);
       my_rule.rule = { h1: { backgroundColor: "beige" } };
+      action_display.text = 'Reset rule completely.'
     },
     /* Reset rule, but selector */
     async () => {
       await delay(DELAY);
       my_rule.rule = { color: "red" };
+      action_display.text = 'Reset rule, but selector.'
     },
     /* Remove rule */
     async () => {
       await delay(DELAY);
       my_rule.remove();
+      action_display.text = 'Removed rule (no CSS).'
     },
     /* Apply clone (restores original) */
     async () => {
       await delay(DELAY);
       my_sheet.append(my_rule_clone);
+      action_display.text = 'Applied clone (restored original CSS).'
     },
     /* Remove clone */
     async () => {
       await delay(DELAY);
       my_rule_clone.remove();
+      action_display.text = 'Removed clone (no CSS).'
     },
     /* Restore without adding to the DOM */
     async () => {
       await delay(DELAY);
       my_rule_clone.target = my_sheet;
+      action_display.text = 'Assigned target to clone without adding clone to the DOM (restored original CSS).'
     }
   );
 })();
