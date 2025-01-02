9458083a4a327458d7f78eb1429c20eb5ce825bb
Uffe Arlo <uat@aperitas.com>
2025-01-02 21:56:55 +0100

    k

---
+++ app_code/src/tests/_all.js
@@ -0,0 +1,44 @@
+// _all
+// import "@/tests/_all"
+
+/* Data */
+await import("@/tests/data_$");
+await import("@/tests/data_change");
+await import("@/tests/data_clear");
+await import("@/tests/data_clone");
+await import("@/tests/data_computed");
+await import("@/tests/data_condition");
+await import("@/tests/data_effect");
+await import("@/tests/data_effects");
+await import("@/tests/data_filter");
+await import("@/tests/data_map");
+await import("@/tests/data_reduce");
+await import("@/tests/data_reset");
+await import("@/tests/data_text");
+await import("@/tests/data_update");
+/* List */
+await import("@/tests/list_$");
+
+await import("@/tests/list_clear");
+await import("@/tests/list_clone");
+await import("@/tests/list_computed");
+await import("@/tests/list_condition");
+await import("@/tests/list_difference");
+await import("@/tests/list_effects");
+await import("@/tests/list_filter");
+await import("@/tests/list_for_each");
+await import("@/tests/list_freeze");
+await import("@/tests/list_has");
+await import("@/tests/list_includes");
+await import("@/tests/list_intersection");
+await import("@/tests/list_map");
+await import("@/tests/list_match");
+await import("@/tests/list_reduce");
+await import("@/tests/list_symmetric_difference");
+await import("@/tests/list_text");
+await import("@/tests/list_transformer");
+await import("@/tests/list_union");
+/* Value */
+await import("@/tests/value_effects");
+
+
