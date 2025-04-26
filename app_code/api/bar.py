f21ec0fd91ee19619c4d5d9671e534b9948c61d5
Uffe Arlo <uat@aperitas.com>
2025-04-26 09:44:48 +0200

    q

--- app_code/api/bar.py
+++ app_code/api/bar.py
@@ -5,10 +5,8 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
-        response = requests.get("https://api.example.com/data")
+        response = requests.get("https://api.github.com/zen")
         data = response.text
-
-
 
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
