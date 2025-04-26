e9859c9e1d7a27fe66ce3c1f07544591c2d1f423
Uffe Arlo <uat@aperitas.com>
2025-04-26 08:06:44 +0200

    q

--- app_code/src/main.js
+++ app_code/src/main.js
@@ -6,18 +6,20 @@ import { modules } from "@/modules.js";
 
 document.querySelector("html").dataset.bsTheme = "dark";
 
-
-
 const { component } = await modules.get("@/rollo/component/component.js");
 
 const { Check } = await modules.get("@/rolloui/components/form/check.js");
 
 document.body.append(Check());
 
-console.dir(location)
+console.dir(location);
 
-const response = await fetch('api/foo');
-console.log(await response.text())
+if (["testapp8.vercel.app", "localhost:3000"].includes(location.host)) {
+  const response = await fetch("api/foo");
+  console.log(await response.text());
+}
+
+
 
 //const response = await fetch('/.netlify/functions/foo');
 //console.log(await response.text())
