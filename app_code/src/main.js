0dd014b6a547e555b57d5dfb99aab2f917300f64
Uffe Arlo <uat@aperitas.com>
2025-04-27 17:18:18 +0200

    k

--- app_code/src/main.js
+++ app_code/src/main.js
@@ -1,7 +1,7 @@
 import { vercel } from "@/rollovercel/vercel.js";
 
-console.log('Vercel environment:', vercel.environment.NAME)
-console.log('Vercel url:', vercel.URL)
+//console.log('Vercel environment:', vercel.environment.NAME)
+//console.log('Vercel url:', vercel.URL)
 
 /* NOTE Do NOT await import! */
 /* Make a clean distinction between development and production Vite envs */
