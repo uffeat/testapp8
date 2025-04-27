1911c7a02768e0f327ac941cdb2b603697d7b30e
Uffe Arlo <uat@aperitas.com>
2025-04-27 14:23:41 +0200

    Backup before branching

--- app_code/src/main.js
+++ app_code/src/main.js
@@ -1,6 +1,7 @@
 
 
 /* NOTE Do NOT await import! */
+/* Make a clean distinction between development and production Vite envs */
 if (import.meta.env.DEV) {
   import("@/main/development/main.js");
 } else {
