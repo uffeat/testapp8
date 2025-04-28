d92bd0895baa9cced8d27fabd8e505189f0a1d28
Uffe Arlo <uat@aperitas.com>
2025-04-28 22:32:35 +0200

    q

---
+++ pyscript_code/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+/* Create __dirname to help aliases work reliably */
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => {
+  return {
+    /* Set base URL for dev and production */
+    base: mode === "production" ? "./" : "/",
+    build: {
+      /* Enable manifest.json generation */
+      manifest: true, //
+      /* Enable features like top-level await, async imports, and smaller output */
+      target: "es2022",
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
+    },
+  };
+});
