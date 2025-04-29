d7329bbf3c24662008539fd51f4b79686c812010
Uffe Arlo <uat@aperitas.com>
2025-04-29 10:23:17 +0200

    k

---
+++ lib_code/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "MyLib",
+      fileName: (format) => `mylib.${format}.js`,
+      formats: ["es"],
+    },
+    target: "es2022",
+    manifest: false,
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {},
+        inlineDynamicImports: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
