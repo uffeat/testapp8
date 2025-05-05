dab7213fb9a897c13807b0300ff3eebc7a4cab4f
Uffe Arlo <uat@aperitas.com>
2025-05-05 09:22:52 +0200

    k

--- app_code/src/main/development/main.js
+++ app_code/src/main/development/main.js
@@ -5,7 +5,10 @@ import "@/main.css";
 //import { modules } from "@/rollovite/modules.js";
 
 import { component } from "@/rollo/component/component.js";
+/*
 import { Check } from "@/rolloui/components/form/check.js";
+*/
+const { Check } = await modules.get("@/rolloui/components/form/check.js")
 
 console.info("Vite environment:", import.meta.env.MODE);
 
