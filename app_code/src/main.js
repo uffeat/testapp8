8d1f2ea702cf5a922b933896b67d913672a0bdf3
Uffe Arlo <uat@aperitas.com>
2025-05-05 15:10:48 +0200

    modules seems good

--- app_code/src/main.js
+++ app_code/src/main.js
@@ -1,11 +1,11 @@
-import { modules } from "@/rollovite/modules.js";
+import "@/rollovite/modules.js";
 
 /* NOTE Do NOT await import! */
 if (import.meta.env.DEV) {
   import("@/main/development/main.js");
-  //modules.get("@/main/development/main.js")
+ 
 
 } else {
   import("@/main/production/main.js");
-  //modules.get("@/main/production/main.js")
+ 
 }
