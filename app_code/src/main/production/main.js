b21fb92b1bd4fcb82a41a7dbb371e6c40393c4ca
Uffe Arlo <uat@aperitas.com>
2025-05-06 12:53:29 +0200

    k

--- app_code/src/main/production/main.js
+++ app_code/src/main/production/main.js
@@ -11,7 +11,7 @@ document.querySelector("html").dataset.bsTheme = "dark";
 
 document.body.append(Check());
 
-const { foo } = await modules.import.public.test.foo.foo.js()
+const { foo } = await modules.import.src.test.foo.foo.js()
 console.log('foo:', foo)
 
 const raw_foo = await modules.import.src.test.foo.foo.js({raw: true})
@@ -25,52 +25,5 @@ NOTE
   e.g., serverless functions. All other tests should be relegated to 
   main/development/tests  */
 if (!vercel.environment.PRODUCTION) {
-  /* Batch tests */
-  (async () => {
-    const loaders = import.meta.glob(
-      "/src/main/production/tests/batch/**/*.test.js"
-    );
-
-    window.addEventListener("keydown", async (event) => {
-      if (event.code === "KeyT" && event.shiftKey) {
-        let count = 0;
-
-        for (const [path, load] of Object.entries(loaders)) {
-          const module = await load();
-          const tests = Object.values(module);
-          for (const test of tests) {
-            count++;
-            test();
-          }
-        }
-        console.info(`Invoked ${count} test functions.`);
-      }
-    });
-  })();
-
-  /* Single-file tests */
-  (() => {
-    const loaders = import.meta.glob(
-      "/src/main/production/tests/**/*.test.js"
-    );
-
-    let path = "";
-
-    window.addEventListener("keydown", async (event) => {
-      if (event.code === "KeyU" && event.shiftKey) {
-        path = prompt("Path:", path);
-        if (path) {
-          const load = loaders[`/src/main/production/tests/${path}.test.js`];
-          if (!load) {
-            throw new Error(`Invalid path: ${path}`);
-          }
-          const module = await load();
-          const tests = Object.values(module);
-          for (const test of tests) {
-            test(true);
-          }
-        }
-      }
-    });
-  })();
+  
 }
