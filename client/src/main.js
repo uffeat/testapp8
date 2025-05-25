d7a4f441aaa0fd0e0bd735c0336da1c754165e92
Uffe Arlo <uat@aperitas.com>
2025-05-25 12:45:19 +0200

    direct import

--- client/src/main.js
+++ client/src/main.js
@@ -8,12 +8,14 @@ import "@/bootstrap.scss";
 import "@/main.css";
 import "@/rollovite/__init__.js";
 
+const constructed = Function('path', 'return import(path)')
+
 //VERCEL_URL
 //BASE_URL
 const import_ = async (path) => {
   // NOTE No caching
   //path = `${import.meta.env.VERCEL_URL || ''}${path}`;
-  path = import.meta.env.DEV ? path : `.${path}`
+  path = import.meta.env.DEV ? path : `.${path.slice('/assets'.length)}`
 
 
   const module = await new Function(`return import("${path}")`)();
