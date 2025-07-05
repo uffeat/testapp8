ff9b0c6e8285bc768486d745f4cd637941aa330d
Uffe Arlo <uat@aperitas.com>
2025-07-05 22:47:11 +0200

    q

--- client/src/main.js
+++ client/src/main.js
@@ -12,26 +12,3 @@ const { component } = await use("@/rollocomponent/");
 
 const { anvil } = await use("@/rolloanvil/");
 
-anvil.channels.add("foo", (data) => {
-  console.log("foo channel got data:", data);
-});
-
-await (async () => {
-  const response = await anvil.client.echo({ number: 42 });
-  console.log("client response:", response);
-})();
-
-await (async () => {
-  const response = await anvil.client.echo({ number: 42 });
-  console.log("client response:", response);
-})();
-
-await (async () => {
-  const response = await anvil.client.foo("To foo");
-  console.log("client response:", response);
-})();
-
-await (async () => {
-  const response = await anvil.client.foo("To foo");
-  console.log("client response:", response);
-})();
