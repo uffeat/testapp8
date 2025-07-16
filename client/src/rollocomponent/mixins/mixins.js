c3841a48699cad075182bbdc7ea7601d598bfd0c
Uffe Arlo <uat@aperitas.com>
2025-07-16 10:51:18 +0200

    q

--- client/src/rollocomponent/mixins/mixins.js
+++ client/src/rollocomponent/mixins/mixins.js
@@ -1,47 +1,64 @@
 /*
 import { mixins } from "@/rollocomponent/mixins/mixins.js";
 const { mixins } = await use("@/rollocomponent/mixins/");
-
 */
 
-const names = [
-  "append",
-  "attrs",
-  "classes",
-  "clear",
-  "components",
-  "connect",
-  "detail",
-  "find",
-  "for_",
-  "handlers",
-  "hooks",
-  "host",
-  "host",
-  "insert",
-  "key",
-  "novalidation",
-  "parent",
-  'props',
-  "send",
-  "setup",
-  "states",
-  "style",
-  "super_",
-  "tab",
-  "tree",
-  "text",
-  "vars",
-];
+import append from "./mixins/append.js";
+import attrs from "./mixins/attrs.js";
+import classes from "./mixins/classes.js";
+import clear from "./mixins/clear.js";
+import components from "./mixins/components.js";
+import connect from "./mixins/connect.js";
+import detail from "./mixins/detail.js";
+import find from "./mixins/find.js";
+import for_ from "./mixins/for_.js";
+import handlers from "./mixins/handlers.js";
+import hooks from "./mixins/hooks.js";
+import host from "./mixins/host.js";
+import insert from "./mixins/insert.js";
+import key from "./mixins/key.js";
+import novalidation from "./mixins/novalidation.js";
+import parent from "./mixins/parent.js";
+import props from "./mixins/props.js";
+import send from "./mixins/send.js";
+import setup from "./mixins/setup.js";
+import states from "./mixins/states.js";
+import style from "./mixins/style.js";
+import super_ from "./mixins/super_.js";
+import tab from "./mixins/tab.js";
+import tree from "./mixins/tree.js";
+import text from "./mixins/text.js";
+import vars from "./mixins/vars.js";
 
-const mixins = {};
-for (const name of names) {
-  mixins[name] = (
-    await use(`@/rollocomponent/mixins/mixins/${name}.js`)
-  ).default;
-}
+const mixins = {
+  append,
+  attrs,
+  classes,
+  clear,
+  components,
+  connect,
+  detail,
+  find,
+  for_,
+  handlers,
+  hooks,
+  host,
+  insert,
+  key,
+  novalidation,
+  parent,
+  props,
+  send,
+  setup,
+  states,
+  style,
+  super_,
+  tab,
+  tree,
+  text,
+  vars,
+};
+
 Object.freeze(mixins);
-
-
 
 export { mixins };
